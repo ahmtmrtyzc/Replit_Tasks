31ef5d98d5dbe13f3f6248d9e8ff7932d693fd60
ahmet yazici <64504928+ahmtmrtyzc@users.noreply.github.com>
2023-08-04 08:25:34 -0400

    Add files via upload

---
+++ AverageTemperature.java
@@ -0,0 +1,16 @@
+package ReplitTries;
+
+import java.util.Scanner;
+
+public class AverageTemperature {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		double[] temps = { scan.nextDouble(), scan.nextDouble(), scan.nextDouble(), scan.nextDouble(),
+				scan.nextDouble(), scan.nextDouble(), scan.nextDouble(), scan.nextDouble() };
+
+		double ave = (temps[0]+temps[1]+temps[3]+temps[4]+temps[5]+temps[6]+temps[7])/8;
+		System.out.println(ave);
+	}
+
+}
