7b946e4d874b8e5ed3a3c67f2bd5a54c12c05e09
ahmet yazici <64504928+ahmtmrtyzc@users.noreply.github.com>
2023-09-03 22:12:34 -0400

    Update CountEven.java

--- CountEven.java
+++ CountEven.java
@@ -1,10 +1,20 @@
-package ReplitTries;
+//Given an array **nums**, count and print the number of even numbers in the array//package ReplitTries;
 
 public class CountEven {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner input = new Scanner(System.in);
+		int[] nums = { input.nextInt(), input.nextInt(), input.nextInt(), input.nextInt(), input.nextInt() };
+		
 
+		int even = 0;
+		for (int i = 0; i < nums.length; i++) {
+			if (nums[i] % 2 == 0) {
+				even++;
+			}
+			System.out.println(even);
+
+		}
 	}
 
 }
