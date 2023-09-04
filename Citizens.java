e3b04ff2168389b078cea60ced35c3273bef2192
ahmet yazici <64504928+ahmtmrtyzc@users.noreply.github.com>
2023-09-03 22:10:11 -0400

    Update Citizens.java

--- Citizens.java
+++ Citizens.java
@@ -1,3 +1,24 @@
+//1. Declare int variables: seniorCitizens, nonSeniorCitizens, age
+//2. Create a Scanner object
+//3. Ask user to enter count for seniorCitizens and nonSeniorCitizens using message:
+
+// "Enter current count for seniorCitizens and nonSeniorCitizens:"
+
+//4. Ask user to enter age:
+
+//  "What is new citizen's age?"
+
+//- if citizen's age is more than or equals to 65 then print "Senior Citizen" and increment seniorCitizens variable by 1.
+
+//- if citizen's age is less than 65 then print "Non-Senior Citizen" and increment nonSeniorCitizens variable by 1.
+
+//5. Last print another message:
+
+//  "New seniorCitizens count ValueOfVariable"
+
+//  "New nonSeniorCitizens count ValueOfVariable"
+
+
 package ReplitTries;
 
 import java.util.Scanner;
